6f68a444f4576018112605ca4d945e853f954bc4
ehddnr301 <dy950328@gmail.com>
2021-03-11 13:44:27 +0900

    feat - searchInfo UI

--- src/components/SearchInfo.js
+++ src/components/SearchInfo.js
@@ -1,5 +1,40 @@
 export default class SearchInfo {
-  constructor() {}
+  constructor($target, data) {
+    this.$target = $target;
+    this.data = data;
 
-  render() {}
+    this.render();
+  }
+
+  render() {
+    const { url, temperament, origin } = this.data;
+
+    const modal = document.createElement("div");
+    modal.className = "modal";
+
+    const info = document.createElement("div");
+    info.className = "info";
+
+    const infoImg = document.createElement("img");
+    infoImg.src = url;
+
+    const infoDescription = document.createElement("div");
+    infoDescription.className = "info-description";
+
+    const infoTemperament = document.createElement("p");
+    infoTemperament.className = "info-temperament";
+    infoTemperament.innerText = temperament;
+
+    const infoOrigin = document.createElement("p");
+    infoOrigin.className = "info-origin";
+    infoOrigin.innerText = origin;
+
+    infoDescription.appendChild(infoTemperament);
+    infoDescription.appendChild(infoOrigin);
+    info.appendChild(infoImg);
+    info.appendChild(infoDescription);
+    modal.appendChild(info);
+
+    this.$target.appenChild(modal);
+  }
 }
