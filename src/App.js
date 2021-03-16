3d014418baa46b636925ac095234adcc027f97f6
ehddnr301 <dy950328@gmail.com>
2021-03-16 16:52:59 +0900

    feat - Make spinner

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import SearchingSection from "./components/SearchingSection.js";
 import { api } from "./api/theCatAPI.js";
 import ResultsSections from "./components/ResultsSection.js";
 import DetailModal from "./components/DetailModal.js";
+import Loading from "./components/Loading.js";
 
 export default class App {
   constructor($target) {
     const searchingSection = new SearchingSection({
       $target,
       onSearch: (keyword) => {
+        loading.toggleSpinner();
         api.fetchCats(keyword).then((data) => {
+          loading.toggleSpinner();
+
           resultsSection.setState(data);
         });
       },
       onRandom: () => {
+        loading.toggleSpinner();
         api.fetchRandomCats().then((data) => {
+          loading.toggleSpinner();
+
           resultsSection.setState(data);
         });
       },
@@ -27,6 +34,10 @@ export default class App {
     });
 
     const detailModal = new DetailModal({
+      $target,
+    });
+
+    const loading = new Loading({
       $target,
     });
 
